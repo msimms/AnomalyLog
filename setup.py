91807f66aa5fcd3589b910622405bc47d08fd149
Mike Simms <msimms01@gmail.com>
2019-07-30 12:24:30 -0400

    Added setup.py.

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup, find_packages
+
+requirements = []
+
+setup(
+    name='anomalylog',
+    version='0.1.0',
+    description='',
+    url='https://github.com/msimms/AnomalyLog',
+    author='Mike Simms',
+    packages=find_packages(exclude=['contrib', 'docs', 'tests']),
+    install_requires=requirements,
+)
